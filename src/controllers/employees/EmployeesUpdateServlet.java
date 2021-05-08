fd4e5bf24fed29716517349561c11e0985b036c4
sakitaren <rensakita4219@gmail.com>
2021-05-08 18:28:09 +0900

    Add Employee destroy

--- src/controllers/employees/EmployeesUpdateServlet.java
+++ src/controllers/employees/EmployeesUpdateServlet.java
@@ -41,6 +41,7 @@ public class EmployeesUpdateServlet extends HttpServlet {
 		if(_token != null &&_token.equals(request.getSession().getId())) {
 			EntityManager em = DBUtil.createEntityManager();
 
+			
 
 			Employee e = em.find(Employee.class, (Integer)(request.getSession().getAttribute("employee_id")));
 
